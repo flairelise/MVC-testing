e74f1d4a038fb0cc20d646c967008e356aeefb03
Carys Brown <ceb024@uark.edu>
2021-12-06 18:29:31 -0600

    added movies page, deployable locally only

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,16 +9,24 @@ using BusinessDevProject.Models;
 using System.Data.SqlClient;
 using System.Data.SqlTypes;
 using WebMatrix.Data;
+using System.Configuration;
 
 namespace BusinessDevProject.Controllers
 {
     public class HomeController : Controller
     {
+        SqlCommand com = new SqlCommand();
+        SqlDataReader dr;
+        SqlConnection con = new SqlConnection();
+        List<Movies> movies = new List<Movies>();
+        
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            con.ConnectionString = BusinessDevProject.Properties.Resources.ConnectionString;
         }
 
         public IActionResult Index()
@@ -38,9 +46,42 @@ namespace BusinessDevProject.Controllers
 
         public IActionResult MovieList()
         {
-            return View();
+            FetchData();
+            return View(movies);
         }
-
+        private void FetchData()
+        {
+            if (movies.Count > 0)
+            {
+                movies.Clear();
+            }
+            try
+            {
+                con.Open();
+                com.Connection = con;
+                com.CommandText = "SELECT TOP (1000) Movie_Name, Streamable, Release_Date, Duration FROM Movie ORDER BY Movie_Name";
+                dr = com.ExecuteReader();
+                while (dr.Read())
+                {
+                    movies.Add(new Movies()
+                    {
+                        Movie_Name = dr["Movie_Name"].ToString()
+                    ,
+                        Streamable = dr["Streamable"].ToString()
+                    ,
+                        Release_Date = dr["Release_Date"].ToString()
+                    ,
+                        Duration = dr["Duration"].ToString()
+                    
+                    });
+                }
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ Models/MovieModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BusinessDevProject.Models
+{
+    public class Movies
+    {
+        public string Movie_Name { get; set; }
+        public string Streamable { get; set; }
+        public string Release_Date { get; set; }
+        public string Duration { get; set; }
+
+    }
+}
